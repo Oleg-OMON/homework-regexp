49ce115cfdc934e6793d637f639f693251747436
Oleg Zasedatelev <ol.zasedatelew@yandex.ru>
2022-07-02 21:48:49 +0300

    file added

--- src/js/__test__/app.test.js
+++ src/js/__test__/app.test.js
@@ -2,7 +2,7 @@ import Validator from "../app";
 test('Получим наше имя', () => {
     const validator = new Validator();
     const result = validator.validateUsername('w111R-e_y3a');
-    expect(result).toBe('w111R-e_y3a');
+    expect(result).toBe('w11R-e_y3a');
   });
   
   test('ошибка присутствия кирилицы', () => {
--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,27 @@
 /* eslint-disable no-useless-escape */
 export default class Validator{
     validateUsername(name) {
-    const reNumbers = /\d{3}/;
-    const re = /^[a-zA-Z0-9\_\-]+$/;
-    const reBeginningEnding = /^[\d\-\_]|[\d\-\_]$/
+        const reNumbers = /\d{3}/;
+        const re = /^[a-zA-Z0-9\_\-]+$/;
+        const reBeginningEnding = /^[\d\-\_]|[\d\-\_]$/
 
 
-    if (name.search(re) === -1){
-        throw new Error('Допустимы только латинские буквы, символы тире -, подчёркивания _ и цифры (0-9)');
+        if (name.search(re) === -1){
+            throw new Error('Допустимы только латинские буквы, символы тире -, подчёркивания _ и цифры (0-9)');
+            }
+            
+        
+        if (name.match(reNumbers) !== null){
+            throw new Error('Имя не должно содержать подряд более трёх цифр');
         }
         
-    
-    if (name.match(reNumbers) !== null){
-        throw new Error('Имя не должно содержать подряд более трёх цифр');
-    }
-    
 
-    if (name.match(reBeginningEnding) !== null){
-        throw new Error('Имя не должно начинаться и заканчиваться цифрами, символами подчёркивания или тире');
-    }
-    
-    this.name = name;
-    return this.name;
+        if (name.match(reBeginningEnding) !== null){
+            throw new Error('Имя не должно начинаться и заканчиваться цифрами, символами подчёркивания или тире');
+        }
+        
+        this.name = name;
+        return this.name;
         
     }
 
